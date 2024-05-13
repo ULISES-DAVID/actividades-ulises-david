59a6e9cb2320b8274367c8ae495d92b76a69ef1d
ULISES-DAVID <168507255+ULISES-DAVID@users.noreply.github.com>
2024-05-13 11:36:53 -0600

    Add files via upload

---
+++ david2BMPG/ACTIVIDADES/examen_practicover2_13_05_2024.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+    char password[] = {'2', 'b', 'm', '3', '0', '\0'};
+    char input_password[20];
+    char name[50];
+    char grupo[10];
+    int remaining_attempts = 3;
+
+    printf("Hola, ¿cómo te llamas? ");
+    scanf("%s", name);
+    printf("ingresa tu salon ");
+    scanf("%s",grupo);
+
+    int i = 0;
+    while (i < 3) {
+        printf("Ingresa tu contraseña (te quedan %d intentos): ", 3 - i);
+        scanf("%s", input_password);
+
+        int j = 0;
+        int match = 1;
+        while (password[j] != '\0' && input_password[j] != '\0') {
+            if (password[j] != input_password[j]) {
+                match = 0;
+                break;
+            }
+            j++;
+        }
+
+        if (match && password[j] == '\0' && input_password[j] == '\0') {
+            printf("¡Contraseña correcta, %s! ¿Cuántas facturas tienes? ", name);
+            int num_facturas;
+            scanf("%d", &num_facturas);
+            
+            printf("Bienvenido, %s. Tienes %d facturas.\n", name, num_facturas);
+            break;
+        } else {
+            printf("Contraseña incorrecta. Prueba de nuevo.\n");
+            i++;
+        }
+    }
+
+    if (i == 3) {
+        printf("Demasiados intentos. Intenta más tarde.\n");
+    }
+
+    return 0;
+}

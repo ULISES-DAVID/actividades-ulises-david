036dcd49b42a0e8d704e54de6d34e0c0e8cc6f5a
ULISES-DAVID <168507255+ULISES-DAVID@users.noreply.github.com>
2024-05-13 11:57:08 -0600

    Add files via upload

---
+++ david2BMPG/ACTIVIDADES/examen_practicover5_13_05_2024.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+int main() {
+    char password[] = {'2', 'b', 'm', '3', '0', '\0'};
+    char input_password[20];
+    char name[50];
+    char grupo[10];
+    int remaining_attempts = 3;
+    int precio;
+    int sumatotal;
+    
+
+    printf("Hola, ¿cómo te llamas? ");
+    scanf("%s", name);
+    printf("ingresa tu salon ");
+    scanf("%s",grupo);
+
+    int i = 0;
+    while (i < 3) {
+        printf("Ingresa tu contraseña (te quedan %d intentos): ", 3 - i);
+        scanf("%s", input_password);
+
+        int j = 0;
+        int match = 1;
+        while (password[j] != '\0' && input_password[j] != '\0') {
+            if (password[j] != input_password[j]) {
+                match = 0;
+                break;
+            }
+            j++;
+        }
+
+        if (match && password[j] == '\0' && input_password[j] == '\0') {
+            printf("¡Contraseña correcta, %s! ¿Cuántas facturas tienes? ", name);
+            int num_facturas;
+            scanf("%d", &num_facturas);
+         
+            if(num_facturas>0){
+                printf("el numero ingresado es positivo");
+            } else {
+                printf("el numero no es positivo ");
+            }
+            
+            printf("ingrese el precio de de la factura ");
+            scanf ("%i", &precio);
+          
+            for (int k = 0; k < num_facturas; k++) {
+                printf("La factura número %d tiene un valor de %d\n", k+1, precio);
+                
+            
+            }
+            
+                    sumatotal=(num_facturas*precio);
+            printf("el total es de %i\n", sumatotal); 
+            break;
+            
+        
+            
+            
+        } else {
+            printf("Contraseña incorrecta. Prueba de nuevo.\n");
+            i++;
+        }
+    }
+
+    if (i == 3) {
+        printf("Demasiados intentos. Intenta más tarde.\n");
+    }
+
+    return 0;
+}
